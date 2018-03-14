aa18593766757831f2350d627d1ce38b345f78ba
eod <eod@naver.com>
2018-03-14 15:40:05 +0900

    3

--- eclipse-workspace/GitTest/src/com/yang/dao/UserDao.java
+++ eclipse-workspace/GitTest/src/com/yang/dao/UserDao.java
@@ -1,5 +1,19 @@
 package com.yang.dao;
 
-public class UserDao {
+import java.util.List;
 
+import com.yang.entity.User;
+
+public class UserDao {
+	List<User> list = null;
+
+	public UserDao() {
+		list.add(new User("홍길동", 17, 75.3));
+		list.add(new User("이순신", 18, 100.0));
+		list.add(new User("강호동", 15, 90.3));
+	}
+
+	public List<User> getUserList() {
+		return list;
+	}
 }
--- eclipse-workspace/GitTest/src/com/yang/entiy/User.java
+++
@@ -1,5 +0,0 @@
-package com.yang.entiy;
-
-public class User {
-
-}
