9e93851fafb44c57cd2cd6f9621195d4d0a8a780
eod <eod@naver.com>
2018-03-14 15:45:17 +0900

    4

--- eclipse-workspace/GitTest/src/com/yang/dao/UserDao.java
+++ eclipse-workspace/GitTest/src/com/yang/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.yang.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.yang.entity.User;
@@ -8,6 +9,7 @@ public class UserDao {
 	List<User> list = null;
 
 	public UserDao() {
+		list = new ArrayList<User>();
 		list.add(new User("홍길동", 17, 75.3));
 		list.add(new User("이순신", 18, 100.0));
 		list.add(new User("강호동", 15, 90.3));
